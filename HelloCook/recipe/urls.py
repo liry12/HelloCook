c4b23176037f40296e9f5575e0e7571caca3145b
liry12 <751276120@qq.com>
2020-04-01 01:32:42 +0100

    remove useless package.

--- HelloCook/recipe/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'recipe'
-
-urlpatterns = [
-     path(r'', views.recipe_list, name='recipe_list'),
-     path(r'<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
-]
